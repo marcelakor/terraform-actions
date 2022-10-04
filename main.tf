6ffe256ed2449bf4022e0f941db550872853a4bc
marcelakor <94497895+marcelakor@users.noreply.github.com>
2022-10-04 17:10:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1" # define region as per your account
 }
 
-resource "aws_s3_bucket" "coucou_sweet" {
+resource "aws_s3_bucket" "co_sweet" {
   bucket = "demo-github-action-tf-medium"
 
   object_lock_enabled = false
